e90dcb12788a87fc6f1bde2b58c2130020112888
yclavinas <yclavinas@gmail.com>
2021-02-17 17:37:22 +0900

    fixed var

--- WANNRelease/prettyNeatWann/neat_src/wann_dataGatherer.py
+++ WANNRelease/prettyNeatWann/neat_src/wann_dataGatherer.py
@@ -73,6 +73,7 @@ class WannDataGatherer():
 
     
     # --- Generation fit/complexity stats ------------------------------------ 
+    print("self.elite[-1].var", self.elite[-1].var)
     self.node_med = np.append(self.node_med,np.median(nodes))
     self.conn_med = np.append(self.conn_med,np.median(conns))
     self.fit_med  = np.append(self.fit_med, np.median(fitness))
@@ -81,7 +82,8 @@ class WannDataGatherer():
     self.fit_max  = np.append(self.fit_max,  self.elite[-1].fitness)
     self.fit_top  = np.append(self.fit_top,  self.best[-1].fitness)
     self.fit_peak = np.append(self.fit_peak, self.best[-1].fitMax)
-    
+    print("sself.best_var", self.best_var)
+    exit()
     # ------------------------------------------------------------------------ 
 
 
