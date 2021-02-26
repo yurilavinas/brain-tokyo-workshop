9e8cd561edd51ad4ef903e9fe37dc0c17b9655d4
yclavinas <yclavinas@gmail.com>
2021-02-26 15:40:51 +0900

    swingup

--- WANNRelease/prettyNeatWann/wann_train.py
+++ WANNRelease/prettyNeatWann/wann_train.py
@@ -262,7 +262,7 @@ def mpi_fork(n):
       IN_MPI="1"
     )
     # print( ["/usr/lib64/openmpi/bin/mpirun ", "-np", str(n), sys.executable] + sys.argv)
-    subprocess.check_call(["mpirun", "-np", str(n), sys.executable] +['-u']+ sys.argv, env=env)
+    subprocess.check_call(["/usr/lib64/openmpi/bin/mpirun", "-np", str(n), sys.executable] +['-u']+ sys.argv, env=env)
     return "parent"
   else:
     global nWorker, rank
