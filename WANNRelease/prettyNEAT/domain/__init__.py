66d0bd26cc98d986fb624b4226ab4d2e267533d3
yclavinas <yclavinas@gmail.com>
2021-03-12 17:02:00 +0900

    rm unrelated code

--- WANNRelease/prettyNEAT/domain/__init__.py
+++
@@ -1,4 +0,0 @@
-from .make_env import make_env
-from .config import games
-from .task_gym import *
-
--- WANNRelease/prettyNEAT/neat_src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ann import *
-from .ind import *
-from .neat import *
-from .dataGatherer import *
-
